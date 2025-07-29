7d977497339a1ea078cad0991336094bd125149d
Connor Aubry <connoraubry@gmail.com>
2025-07-29 16:04:53 -0500

    fixing update time

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,37 @@
 -- Add any additional options here
 
 vim.g._ts_force_sync_parsing = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- tab options
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+-- search options
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- color options
+vim.opt.termguicolors = true
+
+-- scroll options
+vim.opt.scrolloff = 8
+-- vim.opt.signcolumn = "number"
+-- vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
+
+vim.opt.colorcolumn = "80"
+-- vim.opt.clipboard = "unnamed"
