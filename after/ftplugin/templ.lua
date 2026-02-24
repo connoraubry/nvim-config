23c81e79e55cdbe9933115104687dd7f2878b244
Connor Aubry <connoraubry@gmail.com>
2026-02-24 09:31:20 -0600

    fixing issue with templ comments

--- after/ftplugin/templ.lua
+++ after/ftplugin/templ.lua
@@ -1,6 +1,1 @@
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "templ",
-	callback = function()
-		require("Comment.ft").set("templ", "// %s")
-	end,
-})
+require("Comment.ft").set("templ", "// %s")
