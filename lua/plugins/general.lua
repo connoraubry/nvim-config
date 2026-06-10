ff2396924f43fc0931b39389e2f7a8d9cbc80d5c
Connor Aubry <connoraubry@gmail.com>
2026-06-10 17:01:46 -0500

    cleaning up lualine

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -45,19 +45,15 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
 			sections = {
+				lualine_b = { "diagnostics" },
 				lualine_c = {
-					{
-						"filename",
-						path = 1,
-					},
+					{ "filename", path = 1 },
 				},
+				lualine_x = { "filetype" },
 			},
 			inactive_sections = {
 				lualine_c = {
-					{
-						"filename",
-						path = 1,
-					},
+					{ "filename", path = 1 },
 				},
 			},
 		},
